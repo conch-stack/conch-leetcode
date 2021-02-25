3776c22b20b62b6779f881bfa040492b6cbd18ed
jz.zheng <jz.zheng@trip.com>
2021-02-25 18:01:46 +0800

    remove note

---
+++ src/main/java/ltd/beihu/leetcode/interview/tree/package-info.java
@@ -0,0 +1,18 @@
+/**
+ * 树：左小中右大
+ * 二叉搜索树：平均复杂度 O(logn)，最坏复杂度 O(n) - 即只有小元素或只有大元素，一直排成一链
+ * 二叉平衡数：
+ *      红黑树：
+ *      Splay Tree：
+ *      AVL Tree：
+ * KD Tree：
+ *
+ * 前中后续遍历主要看跟节点的位置
+ *      前：根左右
+ *      中：左根右
+ *      后：左右根
+ *
+ * @author Adam
+ * @date 2020/10/14
+ */
+package ltd.beihu.leetcode.interview.tree;
